dc28732cf7b3b909f5d562f10d2dd1c3d908e5d7
neel-1905 <neelnarwadkar@gmail.com>
2023-01-18 13:14:19 +0530

    changed connection string

--- connection.js
+++ connection.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const connection = async () => {
   const client = await MongoClient.connect(
-    `mongodb+srv://neel1905:neel1905@cluster0.abtfoeg.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.abtfoeg.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
   );
-  const db = client.db("Blogs");
+  const db = client.db();
 
   return { db };
 };
