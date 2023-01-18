c276d1c34be4ae6a7216e3a856956936fababf4d
neel-1905 <neelnarwadkar@gmail.com>
2023-01-18 13:18:38 +0530

    port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const { connection } = require("./connection");
 const app = express();
